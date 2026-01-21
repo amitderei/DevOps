0604963bd28f5a23a6fc1b98e92a778153859e15
Amit Derei <amitderei123@gmail.com>
2026-01-21 20:34:53 +0200

    Fix typo in test function name from sumNun to sumNum

--- test_sum.py
+++ test_sum.py
@@ -5,17 +5,17 @@ from sumNumbers import sumNum
 class TestSumNun(unittest.TestCase):
 
     def test_positive_numbers(self):
-        self.assertEqual(sumNun(2, 3), 5)
+        self.assertEqual(sumNum(2, 3), 5)
 
     def test_negative_numbers(self):
-        self.assertEqual(sumNun(-2, -3), -5)
+        self.assertEqual(sumNum(-2, -3), -5)
 
     def test_mixed_numbers(self):
-        self.assertEqual(sumNun(-2, 3), 1)
+        self.assertEqual(sumNum(-2, 3), 1)
 
     def test_zero(self):
-        self.assertEqual(sumNun(0, 5), 5)
-        self.assertEqual(sumNun(0, 0), 0)
+        self.assertEqual(sumNum(0, 5), 5)
+        self.assertEqual(sumNum(0, 0), 0)
 
 
 if __name__ == "__main__":
